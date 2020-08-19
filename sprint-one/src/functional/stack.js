168916e09d68204a5297e3597d2a56c16d8d79c3
vargas055 <juanjo220393@gmail.com>
2020-08-19 19:34:33 -0400

    implemented size check, working on push method

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,22 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var keysArray = Object.keys(storage);
   // Implement the methods below
   someInstance.push = function(value) {
+    //storage[keysArray.length + 1] = value;
+    return this[1] = 'beep';
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
+    if (keysArray.length < 1) {
+      return 0;
+    }
+    return keysArray.length;
   };
-
+  console.log(someInstance);
   return someInstance;
 };
