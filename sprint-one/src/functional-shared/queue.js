72b0ca583a4acc531031b832428239498a054f77
vargas055 <juanjo220393@gmail.com>
2020-08-20 19:45:35 -0400

    Completed Functional shared!

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var result = {};
+  result.storage = {};
+  result.queueCount = 0;
+  result.line = [];
+  _.extend(result, queueMethods);
+  return result;
 };
 
 var queueMethods = {};
-
+queueMethods.size = function() {
+  return this.line.length;
+};
+queueMethods.enqueue = function(value) {
+  this.queueCount++;
+  this.line.push(this.queueCount);
+  this.storage[this.queueCount] = value;
+};
+queueMethods.dequeue = function(value) {
+  var gone = this.storage[this.line[0]];
+  delete this.storage[this.line[0]];
+  this.line.shift();
+  return gone;
+};
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  /////
+  var result = {
+  };
+  result.storage = {};
+  result.counterStack = 0;
+  _.extend(result, stackMethods);
+  return result;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() {
+    return this.counterStack;
+  },
+  push: function(value) {
+    this.counterStack++;///******* */
+    this.storage[this.counterStack] = value;
+  },
+  pop: function(value) {
+    if (this.counterStack > 0) {
+      var pop = this.storage[this.counterStack];
+      this.counterStack--;
+      return pop;
+    }
+  }
+};
+
+
+
 
 
