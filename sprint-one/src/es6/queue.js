d285d87db1a1bc44f6eda3a88015342c2937a742
vargas055 <juanjo220393@gmail.com>
2020-08-21 11:35:04 -0400

    Completed Sprint1!!!!!

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,24 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.counter = 0;
+    this.line = [];
   }
-
+  size() {
+    return this.line.length;
+  }
+  enqueue(value) {
+    this.counter++;
+    this.line.push(this.counter);
+    this.storage[this.counter] = value;
+  }
+  dequeue(value) {
+    if (this.line.length > 0) {
+      var gone = this.storage[this.line[0]];
+      delete this.storage[this.line[0]];
+      this.line.shift();
+      return gone;
+    }
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,28 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.counter = 0;
+  }
+  size() {
+    return this.counter;
   }
 
+  push(value) {
+    this.counter++;
+    this.storage[this.counter] = value;
+  }
+
+  pop(value) {
+    if (this.counter > 0 ) {
+      var popped = this.storage[this.counter];
+      delete this.storage[this.counter];
+      this.counter--;
+      return popped;
+    }
+  }
+
+
 }
+
+
